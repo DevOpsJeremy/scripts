2edc9a60fd90f1a5031f9e66aaee408d6995dd8d
DevOps Jeremy <142703512+DevOpsJeremy@users.noreply.github.com>
2026-07-17 21:41:32 -0700

    Check for Microsoft packages

--- android/setup.sh
+++ android/setup.sh
@@ -3,9 +3,12 @@
 set -euox
 
 . /etc/os-release
-curl -sSk https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb -o m.deb
-sudo dpkg -i m.deb
-rm m.deb
+
+if [ ! "$(sudo dpkg --get-selections packages-microsoft-prod)" ]; then
+    curl -sSk https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb -o m.deb
+    sudo dpkg -i m.deb
+    rm m.deb
+fi
 
 sudo apt update && \
   sudo apt install -y \
