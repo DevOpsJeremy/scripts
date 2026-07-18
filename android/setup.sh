fd1c7054e9c9286ff2e5504db7ab0958566ef633
DevOps Jeremy <142703512+DevOpsJeremy@users.noreply.github.com>
2026-07-17 22:30:35 -0700

    Update setup.sh

--- android/setup.sh
+++ android/setup.sh
@@ -4,18 +4,18 @@ set -euox
 
 . /etc/os-release
 
-sudo mkdir -p -m 755 /etc/apt/keyrings \
-	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
-	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y
+if dpkg-query -W -f='${Status}' gh 2>/dev/null | grep -q "^install ok installed$"; then
+  sudo mkdir -p -m 755 /etc/apt/keyrings && \
+  out=$(mktemp) && curl -sSk -o $out https://cli.github.com/packages/githubcli-archive-keyring.gpg && \
+  cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null && \
+  sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg && \
+  sudo mkdir -p -m 755 /etc/apt/sources.list.d && \
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+fi
 
-if [ ! "$(sudo dpkg --get-selections packages-microsoft-prod)" ]; then
-    curl -sSk https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb -o m.deb
-    sudo dpkg -i m.deb
+if dpkg-query -W -f='${Status}' powershell 2>/dev/null | grep -q "^install ok installed$"; then
+    curl -sSk https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb -o m.deb && \
+    sudo dpkg -i m.deb && \
     rm m.deb
 fi
 
@@ -24,5 +24,6 @@ sudo apt update && \
   git-all \
   python3 \
   python3-venv \
-  powershell
+  powershell \
+  gh
 
