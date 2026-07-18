67444b51ef4329d2c212c06ad7a21886aef66c35
DevOps Jeremy <142703512+DevOpsJeremy@users.noreply.github.com>
2026-07-17 22:11:50 -0700

    Update setup.sh

--- android/setup.sh
+++ android/setup.sh
@@ -4,6 +4,15 @@ set -euox
 
 . /etc/os-release
 
+sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
 if [ ! "$(sudo dpkg --get-selections packages-microsoft-prod)" ]; then
     curl -sSk https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb -o m.deb
     sudo dpkg -i m.deb
