29a0cba2c69d3a0003bd1b63207f40ebc1e58740
DevOps Jeremy <142703512+DevOpsJeremy@users.noreply.github.com>
2026-07-17 20:36:21 -0700

    android install packages

--- android/setup.sh
+++ android/setup.sh
@@ -1,4 +1,16 @@
 #!/use/bin/env sh
 
+set -euox
+
 . /etc/os-release
-cat /etc/os-release
+curl -k https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb -o m.deb
+sudo dpkg -i m.deb
+rm m.deb
+
+sudo apt update && \
+  sudo apt install -y \
+  git-all \
+  python3 \
+  python3-venv \
+  powershell
+
